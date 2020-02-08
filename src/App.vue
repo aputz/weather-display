c72d7fbe0a403962693ba19063dba0209247f471
Agnieszka Putz <putz.agnieszka@gmail.com>
2020-02-08 21:20:00 +0100

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app" class="app__container">
+    <Navbar :appName="appName" />
     <router-view/>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "assets/custom";
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
 
 .app {
   &__container {
     width: 100%;
     height: 100%;
     min-height: 100vh;
-    display: flex;
-    padding: 2rem;
     background-image:  url('assets/bg.jpg');
     background-color: $primary;
     background-size: cover;
@@ -23,7 +24,17 @@
 </style>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
+  data () {
+    return {
+      appName: 'Weather display'
+    }
+  },
+  components: {
+    Navbar
+  },
   watch: {
     '$route' (newValue, oldValue) {
       document.title = newValue.meta.title || 'Weather display'
---
+++ src/components/Logo.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 class="title is-6 has-text-white logo__container">{{appName}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  props: {
+    appName: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scopped>
+@import '../assets/custom.scss';
+
+.logo {
+  &__container {
+    text-transform: lowercase;
+    font-family: Comfortaa, sans-serif;
+    text-shadow: 2px 2px 5px transparentize($text, 0.8);
+  }
+}
+</style>
