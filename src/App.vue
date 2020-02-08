e3d15d58fa4a62edb10cd2170b4dec1a68a7ea31
Agnieszka Putz <putz.agnieszka@gmail.com>
2020-02-08 20:03:59 +0100

    layout base

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
-  <div id="app">
+  <div id="app" class="app__container">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "assets/custom";
+
+.app {
+  &__container {
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+    display: flex;
+    padding: 2rem;
+    background-image:  url('assets/bg.jpg');
+    background-color: $primary;
+    background-size: cover;
+    background-blend-mode: soft-light;
+  }
+}
 </style>
+
+<script>
+export default {
+  watch: {
+    '$route' (newValue, oldValue) {
+      document.title = newValue.meta.title || 'Weather display'
+    }
+  },
+  created () {
+    document.title = 'Weather display'
+  }
+}
+</script>
